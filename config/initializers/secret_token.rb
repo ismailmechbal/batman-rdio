3248bd3cd33dbb9bb5572718d8b5eb75ad0a5d32
Nick Small <nick@shopify.com>
2013-02-28 11:10:24 -0500

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rdio::Application.config.secret_key_base = 'c21009a04d683aa29f3756bb2c9e159a61e58ba07150bd53c67c448c9a6993de6fd1a8c83ac75f98bfb9ee8f288477fb554d97e724251d52b43db1c7f6778427'
